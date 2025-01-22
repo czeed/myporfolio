c587039d8333746aa1d31fddc00a9fd93dd28252
czeed <etdaffe13@gmail.com>
2025-01-22 14:48:52 +0100

    changed icon

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 1.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[200px] h-[200px]  xl:h-[498px] xl:w-[498px] mix-blend-lighten absolute"
+          className="w-[200px] h-[200px]  xl:h-[398px] xl:w-[398px] mix-blend-lighten absolute p-3"
         >
           <Image
             src="/assets/photo.png"
@@ -36,12 +36,12 @@ const Photo = () => {
         <motion.svg
           className="w-[300px]   xl:w-[506px] h-[300px]  xl:h-[506px] "
           fill="transparent"
-          viewBox="0 0 506 506"
+          viewBox="0 0 606 606"
           xmlns="http://www.w3.org/2000/svg"
         >
           <motion.circle
-            cx="253"
-            cy="253"
+            cx="250"
+            cy="250"
             r="250"
             stroke="#00ff99"
             strokeWidth="4"
